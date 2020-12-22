2f72a22f9b0825ea1c77d3253f03b0fd97e14d28
direwolf6 <ryanc45@tcd.ie>
2020-12-22 23:18:37 +0000

    creating project

---
+++ TransitIrelandApp/ClientApp/src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export class Home extends Component {
+  static displayName = Home.name;
+
+  render () {
+    return (
+      <div>
+        <h1>My First React ASP.NET Core WebApp</h1>
+        <h3>It's going to be way cooler than your web app, so fuck off with your Redux</h3>
+        </div>
+    );
+  }
+}
