85f5fcfd401dd6613c9abcfbbb2b3b7179f9a3f3
Cade Ryan <caryan@microsoft.com>
2020-12-31 23:02:39 +0000

    adding the periodic web request for gtfs data

---
+++ TransitIrelandApp/GTFS.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using ProtoBuf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using TransitRealtime;
+
+namespace TransitIrelandApp
+{
+    public static class GTFS
+    {
+        public static void UpdateGTFSInformation()
+        {
+            WebRequest request = WebRequest.Create("https://gtfsr.transportforireland.ie/v1/");
+            request.Headers["Cache-Control"] = "no-cache";
+            request.Headers["x-api-key"] = "3fca6259b9814bef8e7a22ea63bdd1ce";
+
+            FeedMessage feed = Serializer.Deserialize<FeedMessage>(request.GetResponse().GetResponseStream());
+
+            File.WriteAllText(Path.Combine(Environment.CurrentDirectory, @"Files\", "GTFS.json"), JsonConvert.SerializeObject(feed));
+            UpdateBusRealtimeData(feed);
+        }
+
+        public static void UpdateBusRealtimeData(FeedMessage feed)
+        {
+        }
+    }
+}
