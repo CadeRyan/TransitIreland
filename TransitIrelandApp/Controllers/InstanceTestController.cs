51128c168e79f6889cd4afa6ddbc6d3d7cdd2cb9
Cade Ryan <caryan@microsoft.com>
2020-12-31 14:44:01 +0000

    testing how controllers are instantiated

---
+++ TransitIrelandApp/Controllers/InstanceTestController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProtoBuf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using TransitRealtime;
+
+namespace TransitIrelandApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InstanceTestController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            int content = Int32.Parse(System.IO.File.ReadAllText(Path.Combine(Environment.CurrentDirectory, @"Files\", "test.txt")));
+            System.IO.File.WriteAllText(Path.Combine(Environment.CurrentDirectory, @"Files\", "test.txt"), (content+1).ToString());
+            return content.ToString();
+        }
+    }
+}
