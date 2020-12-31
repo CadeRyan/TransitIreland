781998eb334b21ec6e24f7b231674f85d682a283
caolanGM <caolangm@gmail.com>
2020-12-31 14:45:06 +0000

    Luas Live Times

--- TransitIrelandApp/ClientApp/src/App.js
+++ TransitIrelandApp/ClientApp/src/App.js
@@ -5,6 +5,8 @@ import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
 import { LuasStopSearch } from './components/Luas/StopSearch';
+import { TrainStopSearch } from './components/Train/StopSearch';
+import { LuasStopRTI } from './components/Luas/StopRTI';
 
 import './custom.css'
 
@@ -18,6 +20,9 @@ export default class App extends Component {
         <Route path='/counter' component={Counter} />
         <Route path='/fetch-data' component={FetchData} />
             <Route path='/luasSearch' component={LuasStopSearch} />
+            <Route path='/luasRTI' component={LuasStopRTI} />
+
+        <Route path='/trainSearch' component={TrainStopSearch} />
       </Layout>
     );
   }
---
+++ TransitIrelandApp/ClientApp/src/components/Train/StopSearch.js
@@ -0,0 +1,67 @@
+﻿import React, { Component } from 'react';
+
+export class TrainStopSearch extends Component {
+    static displayName = TrainStopSearch.name;
+
+    constructor(props) {
+        super(props);
+        this.state = { stops: [], loading: true };
+        this.handleChange = this.handleChange.bind(this);
+
+    }
+
+    handleChange(event) {
+        this.getDataFromBackend(event.target.value);
+    }
+
+    //componentdidmount() {
+    //    this.getdatafrombackend();
+    //}
+
+    static renderResponse(stops) {
+
+        console.log("COUNT", stops)
+        return (
+
+            <ul>
+              {stops.map(stop =>
+                  <li> {stop.Name} </li>
+                )}
+            </ul>
+
+
+        );
+    }
+
+    render() {
+        let contents = this.state.loading
+            ? <p><em>Loading...</em></p>
+            : TrainStopSearch.renderResponse(this.state.stops);
+
+
+
+        return (
+            <div>
+                <h1 id="tabelLabel" >Train Search</h1>
+                <p>This is a train search</p>
+                <form>
+                    <input
+                        type="text"
+                        onChange={this.handleChange}
+                    />
+                </form>
+                {contents}
+            </div>
+        );
+    }
+
+    async getDataFromBackend(input) {
+        const response = await fetch('api/trainsearch/' + input);
+
+        console.log("RESPONSE", response)
+        const data = await response.json();
+        console.log("DATA", data)
+
+        this.setState({ stops: data, loading: false });
+    }
+}
