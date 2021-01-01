3b4bc4b9b5a8635e875aa8289e160a77856a4198
caolanGM <caolangm@gmail.com>
2021-01-01 15:14:42 +0000

    Added Bottom Nav Bar

--- TransitIrelandApp/ClientApp/src/App.js
+++ TransitIrelandApp/ClientApp/src/App.js
@@ -8,22 +8,24 @@ import { LuasStopSearch } from './components/Luas/StopSearch';
 import { TrainStopSearch } from './components/Train/StopSearch';
 import { LuasStopRTI } from './components/Luas/StopRTI';
 
+
 import './custom.css'
 
 export default class App extends Component {
   static displayName = App.name;
 
   render () {
-    return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-            <Route path='/luasSearch' component={LuasStopSearch} />
-            <Route path='/luasRTI' component={LuasStopRTI} />
-
-        <Route path='/trainSearch' component={TrainStopSearch} />
-      </Layout>
+      return (
+          <div className="page-container">
+             <div className="content-wrap">
+                <Route exact path='/' component={Home} />
+                <Route path='/counter' component={Counter} />
+                <Route path='/fetch-data' component={FetchData} />
+                <Route path='/luasSearch' component={LuasStopSearch} />
+                <Route path='/luasRTI' component={LuasStopRTI} />
+                <Route path='/trainSearch' component={TrainStopSearch} />
+              </div>
+          </div>
     );
   }
 }
